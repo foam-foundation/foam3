c93fb4ab203e4f5cf37c76d19dee8eee5672c7bc
Chanmann Lim <chanmannlim@gmail.com>
2025-03-27 21:25:00 -0400

    Update documentation

--- src/foam/core/menu/AuthorizationStatus.js
+++ src/foam/core/menu/AuthorizationStatus.js
@@ -13,22 +13,22 @@ foam.ENUM({
     of a menu, replacing the "authenticate" flag on Menu model.
 
     Menu accessibility table:
-    +-----------------+--------------------------+--------------------+-------------------------+
-    |      Menu       | Unlogged-in (no subject) |     Anonymous      |        Logged-in        |
-    +-----------------+--------------------------+--------------------+-------------------------+
-    | Public          | Yes                      | Yes                | Yes                     |
-    | Authenticated   | Sign-in (redirect)       | Sign-in/Permission | Permission              |
-    | Unauthenticated | Yes                      | Yes                | Default menu (redirect) |
-    +-----------------+--------------------------+--------------------+-------------------------+
+    +-----------------+--------------------+--------------------+-------------------------+
+    |      Menu       | Unlogged-in users  |     Anonymous      |     Logged-in users     |
+    +-----------------+--------------------+--------------------+-------------------------+
+    | Public          | Yes                | Yes                | Yes                     |
+    | Authenticated   | Sign-in (redirect) | Sign-in/Permission | Permission              |
+    | Unauthenticated | Yes                | Yes                | Default menu (redirect) |
+    +-----------------+--------------------+--------------------+-------------------------+
 
-    Public menus are menus that are accessible by anybody. Eg. error-page.
+    Public menus are menus that are accessible by anybody. Eg. faq, toc, etc,.
 
-    Authenticated menus are application user-space menus accessible only by
-    logged-in users. Eg. dashboard, profile, etc., ...
+    Authenticated menus are application menus eg. dashboard, profile, etc.,
+    accessible only by logged-in users.
 
-    Unauthenticated menus are usually "sign-in" and "sign-up" menus that are
-    accessible only when users first landed the app but after successfully
-    sign-in/sign-up the app would redirect the users to default menu, eg.
+    Unauthenticated menus are usually "sign-in" and "sign-up" menus which are
+    accessible only when users first landed on the app. But after successfully
+    sign-in/sign-up, the app would redirect the users to default menu, eg.
     dashboard, that was setup for the user's group.
   `,
 

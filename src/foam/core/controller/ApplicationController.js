95b393fc33fb843ab95e698d2d51e4501a3f8928
sarthak <sarthak@nanopay.net>
2025-05-30 10:46:55 -0400

    Fix stack context on context change

--- src/foam/core/controller/ApplicationController.js
+++ src/foam/core/controller/ApplicationController.js
@@ -467,6 +467,8 @@ foam.CLASS({
         globalThis.x     = self.__subContext__;
         globalThis.MLang = foam.mlang.Expressions.create();
 
+        // Rebuild Stack in correct context
+        self.stack = self.Stack.create({}, self.__subContext__).copyFrom({ ...self.stack, __subSubContext__: undefined });
         self.fetchTheme();
         foam.locale = localStorage.getItem('localeLanguage') || self.theme?.defaultLocaleLanguage || foam.locale;
 

c001ab708e255dfb08a0d56913c038b6ed3cf00a
Joel Hughes <joel@nanopay.net>
2025-06-11 10:46:43 -0400

    FileStore - move magic numbers to static variables

--- src/foam/dao/store/FileStore.js
+++ src/foam/dao/store/FileStore.js
@@ -44,12 +44,15 @@ foam.CLASS({
   ],
 
   javaCode: `
-    protected FileChannel              channel_;
-    protected long                     size_;
-    protected ByteBuffer               buffer_;
-    protected JSONFObjectFormatter     formatter_;
-    protected JSONParser               parser_;
-    final protected int                BUFFER_SIZE  = 1023;
+    protected FileChannel            channel_;
+    protected long                   size_;
+    protected ByteBuffer             buffer_;
+    protected JSONFObjectFormatter   formatter_;
+    protected JSONParser             parser_;
+    final protected CharsetDecoder   decoder_ = StandardCharsets.UTF_8.newDecoder();
+    static final protected int       BUFFER_SIZE = 1024;
+    static final protected byte[]    PREFIX = "p(".getBytes(StandardCharsets.UTF_8);
+    static final protected byte[]    POSTFIX = ")\\n".getBytes(StandardCharsets.UTF_8);
 
     public FileStore(X x, String filename, ClassInfo of)
       throws IOException {
@@ -62,8 +65,8 @@ foam.CLASS({
       } catch ( java.nio.file.FileAlreadyExistsException e ) {
         // nop;
       }
-      Loggers.logger(x, this).info("created", file.toPath());
       channel_   = new RandomAccessFile(file.getPath(), "rw").getChannel();
+      channel_.force(true); // config?
       size_      = channel_.size();
       buffer_    = ByteBuffer.allocate(BUFFER_SIZE);
       formatter_ = new JSONFObjectFormatter(x);
@@ -77,12 +80,15 @@ foam.CLASS({
 
   methods: [
     {
+      documentation: `Calculate the length of the index object and store this
+in the Root marker object, then when later we find the root on a restart,
+we have both the start offset and length to load it.`,
       name: 'storeRoot',
       javaCode: `
         formatter_.reset();
         formatter_.setX(x);
         formatter_.output(obj, getOf());
-        int len = formatter_.builder().length() + 4; // p(...)\n
+        int len = formatter_.builder().length() + PREFIX.length + POSTFIX.length; // p(...)\n
         store(x, new Root(size_, len));
         setRoot(store(x, obj));
         return getRoot();
@@ -98,11 +104,10 @@ foam.CLASS({
         formatter_.output(obj, getOf());
         channel_.position(size_);
         long pos = channel_.position();
-        channel_.write(ByteBuffer.wrap("p(".getBytes(StandardCharsets.UTF_8)));
+        channel_.write(ByteBuffer.wrap(PREFIX));
         channel_.write(ByteBuffer.wrap(formatter_.builder().toString().getBytes(StandardCharsets.UTF_8)));
-        channel_.write(ByteBuffer.wrap(")\\n".getBytes(StandardCharsets.UTF_8)));
+        channel_.write(ByteBuffer.wrap(POSTFIX));
         size_ = channel_.position();
-        channel_.force(true);
         pm.log(x);
         return new FileStored(this, pos, (int) (size_ - pos), obj);
       } catch ( Throwable t ) {
@@ -121,6 +126,7 @@ foam.CLASS({
       PM pm = new PM("FileStore:load");
       FileStored fs = (FileStored) stored;
       try {
+        // NOTE: extra PMs added for intial performance testing
         PM pmBuffer = new PM("FileStore:load:buffer");
         // REVIEW: FileChannel does not support read(buffer, offset, len)
         // so directly allocating buffer on each call for now
@@ -136,8 +142,8 @@ foam.CLASS({
         pmRead.log(x);
         PM pmDecode = new PM("FileStore:load:decode");
         buffer.flip();
-        String decoded = StandardCharsets.UTF_8.newDecoder().decode(buffer).toString();
-        decoded = decoded.substring(2, decoded.length() - 2); // strip p(...)\n
+        String decoded = decoder_.decode(buffer).toString();
+        decoded = decoded.substring(PREFIX.length, decoded.length() - POSTFIX.length); // strip p(...)\n
         pmDecode.log(x);
         PM pmParse = new PM("FileStore:load:parse");
         parser_.setX(x);
@@ -158,13 +164,10 @@ foam.CLASS({
       name: 'findRoot',
       args: 'Context x',
       javaCode: `
-
-      char p = 'p';
+      PM pm = new PM("FileStore:findRoot");
       String classMarker = ":ssalc{(p";
       StringBuffer sb = new StringBuffer();
       ByteBuffer buffer = ByteBuffer.allocate(1);
-      CharsetDecoder decoder = StandardCharsets.UTF_8.newDecoder();
-      PM pm = new PM("FileStore:findRoot");
       try {
         if ( channel_.size() == 0 ) return;
 
@@ -173,7 +176,7 @@ foam.CLASS({
           buffer.clear();
           channel_.read(buffer);
           buffer.flip();
-          String c = decoder.decode(buffer).toString();
+          String c = decoder_.decode(buffer).toString();
           sb.append(c);
           if ( "p".equals(c) ) {
             if ( sb.toString().endsWith(Root.ROOT_MARKER) ) {
@@ -209,6 +212,7 @@ foam.CLASS({
 
       properties: [
         {
+          // REVIEW: this property may not be necessary
           name: 'store',
           class: 'FObjectProperty',
           of: 'foam.dao.store.FileStore'

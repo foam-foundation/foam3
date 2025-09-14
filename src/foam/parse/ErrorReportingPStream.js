973c764cb73855f865822aa76eee9031acddd9db
Vesna Urosev <vesnagreer@gmail.com>
2025-09-14 11:04:05 -0300

    Commented broken code that caused an infinite loop

--- src/foam/parse/ErrorReportingPStream.js
+++ src/foam/parse/ErrorReportingPStream.js
@@ -7,7 +7,8 @@
 foam.CLASS({
   package: 'foam.parse',
   name: 'ErrorReportingPStream',
-  implements: ['foam.parse.JSPStream'],
+  // it breaks the parsers examples, the interface and the class are not compatible
+  //implements: ['foam.parse.JSPStream'],
   documentation: 'PStream decorator to report parsing errors. Used for debugging parsers.',
 
   properties: [

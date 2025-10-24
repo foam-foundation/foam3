36876f8f550fbd7b17ead73066831daf3d2f6e03
Abdelaziz Mahdy <Abdelaziz.h.mahdy@gmail.com>
2025-10-24 14:36:49 -0300

    fix: update copyright information in DateTimeTestModel.js

--- src/foam/lang/test/DateTimeTestModel.js
+++ src/foam/lang/test/DateTimeTestModel.js
@@ -1,6 +1,7 @@
 /**
  * @license
- * Copyright 2025 PayTic. All Rights Reserved.
+ * Copyright 2025 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
  */
 
 foam.CLASS({

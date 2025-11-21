a79f25753e76f804b20869310c7d5418d325f0e3
Anas Amil <anasamil@Anass-MacBook-Air.local>
2025-11-21 11:33:11 +0100

    fixed save new borders on layouts

--- src/foam/core/reflow/LayoutBlock.js
+++ src/foam/core/reflow/LayoutBlock.js
@@ -7,6 +7,7 @@
 foam.CLASS({
   package: 'foam.core.reflow',
   name: 'LayoutUtils',
+  mixins: [ 'foam.u2.StyleConfigurator' ],
   sections: [
     {
       name: 'layoutSettings',
@@ -36,7 +37,9 @@ foam.CLASS({
         this.BORDER,
         this.GRID_COLUMNS,
         this.FLEX_CONTAINER_TYPE,
-        this.FLEX_VALUE
+        this.FLEX_VALUE,
+        this.SHOWN,
+        ...foam.u2.StyleConfigurator.getAxiomsByClass(foam.lang.Property).filter(p => ! p.hidden && ! p.transient)
       ]);
     }
   ],

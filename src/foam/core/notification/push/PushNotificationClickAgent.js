394b5e40d4a523e101faa8ee6a13a066b3272a66
Joel Hughes <joel@nanopay.net>
2025-12-08 16:27:30 -0500

    PushNotificationClienkAgent - support for running without 'window'

--- src/foam/core/notification/push/PushNotificationClickAgent.js
+++ src/foam/core/notification/push/PushNotificationClickAgent.js
@@ -33,6 +33,9 @@ foam.CLASS({
 
   methods: [
     function init() {
+      if ( typeof this.window === 'undefined' )
+        return;
+
       // This agent runs in two phases:
       // 1. if a notification is clicked while the app is closed, the app is opened with the notification data in the URL search params
       // 2. if the app is already open, the notification click event is dispatched by the service worker or ios app

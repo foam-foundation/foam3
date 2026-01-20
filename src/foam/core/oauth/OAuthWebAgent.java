e9c64ebfe7a0f8a1df0c36e66936ae5797e74f71
Chanmann Lim <chanmannlim@gmail.com>
2026-01-19 22:10:33 -0500

    Show oauth exception toast notification
    - after IdP redirect back to us but the app is unable to link the SSO to existing user, it's better to bring back the LoginView as opposed to staying on service/oauth page with error text.

--- src/foam/core/oauth/OAuthWebAgent.java
+++ src/foam/core/oauth/OAuthWebAgent.java
@@ -1,6 +1,7 @@
 package foam.core.oauth;
 
 import foam.core.boot.Boot;
+import foam.core.auth.AuthenticationException;
 import foam.core.session.Session;
 import foam.lang.X;
 import foam.core.http.WebAgent;
@@ -78,7 +79,12 @@ public class OAuthWebAgent implements WebAgent {
             // if an idToken was returned, log the session into the new account
             foam.core.auth.User user;
             if (idToken != null) {
-                user = loginWithIdToken(x, state, provider, tokenResponse.getString("id_token"));
+                try {
+                    user = loginWithIdToken(x, state, provider, tokenResponse.getString("id_token"));
+                } catch (AuthenticationException e) {
+                    resp.sendRedirect("/?oauth_exception=" + e.getMessage());
+                    return;
+                }
             } else {
                 user = session.findUserId(x);
             }
@@ -115,7 +121,7 @@ public class OAuthWebAgent implements WebAgent {
             e.printStackTrace();
             try {
                 resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                resp.getWriter().write("Server error");
+                resp.getWriter().write("Server error: " + e.getMessage());
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
@@ -162,15 +168,15 @@ public class OAuthWebAgent implements WebAgent {
         reader.close();
 
         if (!bodyObject.getBoolean("email_verified")) {
-            throw new foam.core.auth.AuthenticationException("email is not verified");
+            throw new AuthenticationException("Email is not verified");
         }
 
         if (bodyObject.getInt("exp", Integer.MIN_VALUE) < java.time.Instant.now().getEpochSecond()) {
-            throw new foam.core.auth.AuthenticationException("expired token");
+            throw new AuthenticationException("Expired token");
         }
 
         if (!bodyObject.getString("aud").equals(provider.getClientId())) {
-            throw new foam.core.auth.AuthenticationException("incorrect audience");
+            throw new AuthenticationException("Incorrect audience");
         }
 
         String email = bodyObject.getString("email");
@@ -202,7 +208,7 @@ public class OAuthWebAgent implements WebAgent {
 
             foam.dao.DAO userRegistrationDAO = (foam.dao.DAO) x.get("userRegistrationDAO");
             if ( userRegistrationDAO == null ) {
-                throw new RuntimeException("user registration DAO not available");
+                throw new AuthenticationException("userRegistrationDAO not available");
             }
 
             userRegistrationDAO.inX(x).put(builder.build());
@@ -211,12 +217,12 @@ public class OAuthWebAgent implements WebAgent {
         }
 
         if ( user == null ) {
-            throw new RuntimeException("user not found");
+            throw new AuthenticationException("User not found");
         }
 
         foam.core.session.Session session = (foam.core.session.Session)((foam.dao.DAO)x.get("sessionDAO")).find(state.getString("session_id"));
         if ( session == null ) {
-            throw new RuntimeException("session not found");
+            throw new AuthenticationException("Session not found");
         }
 
         foam.core.auth.LoginService login = (foam.core.auth.LoginService)x.get("loginService");

5e9d5dee987732fe3e0a3ded7d87f70ccfe36872
xuerongNanopay <xuerong@nanopay.net>
2026-02-20 11:51:48 -0500

    foam_kv: initial foam key value dao project

---
+++ src/foam/dao/kv/pom.js
@@ -0,0 +1,21 @@
+/**
+ * @license
+ * Copyright 2026 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.POM({
+  name: "kv",
+//   files: [
+//     { name: "LSMDAO", flags: "java" },
+//   ],
+
+//   files: [
+//     { name: "btree/BTreeTest",    flags: "java&test" },
+//     { name: "btree/BTreeObject",  flags: "java" },
+//   ],
+
+  javaFiles: [
+    { name: "KVDAO" },
+  ]
+});

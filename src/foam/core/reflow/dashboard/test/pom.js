189c1d5a5835db38010d2c2d4388eee884282606
Abdelaziz Mahdy <Abdelaziz.h.mahdy@gmail.com>
2026-04-20 17:24:16 -0300

    Scaffold dashboard test directory for sink migration tests

---
+++ src/foam/core/reflow/dashboard/test/pom.js
@@ -0,0 +1,8 @@
+foam.POM({
+  name: 'dashboard-test',
+  files: [
+    { name: 'DashboardAgentShimTest',     flags: 'js&test|java&test' },
+    { name: 'DashboardAgentJsonTest',     flags: 'js&test|java&test' },
+    { name: 'DashboardAgentBaselineTest', flags: 'js&test|java&test' }
+  ]
+});

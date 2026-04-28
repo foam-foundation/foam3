8fb283a343f7a4f16a713377510ca9a2047e7a3d
Joel Hughes <joel@nanopay.net>
2026-04-28 08:08:31 -0400

    CronScheduler now shutdown gracefully to avoid corrupt journal entries.
    CronScheduler was not updated with a 'stop' operation when shutdown
    features were added to foam.
    Note, that it is still the responsibility of any long running agent/service
    started with cron to handle shutdown itself.
    These changes only affect the rescheduling of cron jobs.

--- src/foam/core/cron/CronScheduler.js
+++ src/foam/core/cron/CronScheduler.js
@@ -41,8 +41,14 @@ foam.CLASS({
     'foam.mlang.sink.Sequence',
     'java.util.Date',
     'java.util.List',
-    'java.util.Timer'
+    'java.util.Timer',
+    'java.util.concurrent.atomic.AtomicBoolean'
   ],
+
+  javaCode: `
+  AtomicBoolean running_ = new AtomicBoolean();
+  Object lock_           = new Object();
+  `,
 
   properties: [
     {
@@ -88,9 +94,25 @@ foam.CLASS({
       `
     },
     {
+      name: 'stop',
+      javaCode: `
+      synchronized ( lock_ ) {
+        running_.set(false);
+      }
+      `
+    },
+    {
       name: 'execute',
       javaCode: `
     final Logger logger = Loggers.logger(x, this);
+    synchronized ( lock_ ) {
+      if ( running_.get() ) {
+        logger.warning("already running");
+        return;
+      }
+      running_.set(true);
+    }
+
     try {
       logger.info("initialize", "cronjobs", "start");
       DAO cronJobDAO = (DAO) x.get(getCronJobDAO());
@@ -132,7 +154,7 @@ foam.CLASS({
 
       logger.info("initialize", "cronjobs", "complete");
 
-      while ( true ) {
+      while ( running_.get() ) {
         long delay = getCronDelay();
         if ( getEnabled() ) {
           Date now = new Date();

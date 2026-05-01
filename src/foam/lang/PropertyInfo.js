15e980339ae27a3e438c467c25950a9d8ecbf43a
Joel Hughes <joel@nanopay.net>
2026-05-01 13:09:01 -0400

    Add 'order' to Property/PropertyInfo as need java side for sorting Properties

--- src/foam/lang/PropertyInfo.js
+++ src/foam/lang/PropertyInfo.js
@@ -148,6 +148,7 @@ foam.INTERFACE({
     'void fromCSVLabelMapping(java.util.Map<String,foam.lib.csv.FromCSVSetter> map)',
     'boolean getSheetsOutput() { return false; }',
     'Object castObject(Object value) { return value; }',
+    'int getOrder() { return 0; }',
 
     // REVIEW: FixedWidth PropertyInfo properties/methods. Unsuccessful attempting to add these via refine or extends of foam.lang.PropertyInfo - Joel
     'int fixedWidthOffset() { return 0; }',

de7614a20b09ea55a8d7a0ba47032fdec537fa68
Kevin Greer <kgrgreer@gmail.com>
2026-05-26 15:14:35 -0300

    Fix UserNotificationSink to pass the User instead of the Notification.

--- src/foam/core/notification/UserNotificationSink.js
+++ src/foam/core/notification/UserNotificationSink.js
@@ -47,7 +47,7 @@ with RulerDAO which can perform further per user setup before user.doNotify.`,
       try {
         x = x.put("notification", notification);
         notification.setX(x);
-        if ( notification.getPredicate() != null && ! notification.getPredicate().f(notification) ) {
+        if ( notification.getPredicate() != null && ! notification.getPredicate().f(user) ) {
           return; // Predicate is false, skip this user
         }
       } catch (Exception e) {

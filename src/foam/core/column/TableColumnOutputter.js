foam-foundation/foam3

commit 4213750d040158db0ce202d9ae32465cf1e37f90

Add a DoubleUnitValue type and appropriate views and formatters | Use… (#4773)
* Add a DoubleUnitValue type and appropriate views and formatters | Use the browser formatting for currency when available

* Add number formatting based on locale

* Update tbcf

src/foam/lang/types.js
@@ -909,6 +909,52 @@ foam.CLASS({
       `
     },
     {
+      class: 'Boolean',
+      name: 'hideId',
+      documentation: `
+        If true, the unit id will be excluded when formatting the value by default.
+        NOTE: This only affects formatting when using the unitPropValueToString method using the default view and tableCellFormatter.
+      `
+    },
+    {
+      name: 'unitPropValueToString',
+      value: async function(x, val, unitPropName, excludeUnit) {
+        if ( unitPropName ) {
+          const unitProp = await x.currencyDAO.find(unitPropName);
+          if ( unitProp )
+            return unitProp.format(val, excludeUnit, false);
+        }
+        return val;
+      }
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.lang',
+  name: 'DoubleUnitValue',
+  extends: 'Double',
+  documentation: `
+    A Double value with an associated unit property for formatting.
+  `,
+  properties: [
+    {
+      class: 'String',
+      name: 'unitPropName',
+      documentation: `
+        The name of the property of a model that contains the denomination String.
+      `
+    },
+    {
+      class: 'Boolean',
+      name: 'hideId',
+      documentation: `
+        If true, the unit id will be excluded when formatting the value by default.
+        NOTE: This only affects formatting when using the unitPropValueToString method using the default view and tableCellFormatter.
+      `,
+      value: true
+    },
+    {
       name: 'unitPropValueToString',
       value: async function(x, val, unitPropName, excludeUnit) {
         if ( unitPropName ) {

src/foam/lang/Currency.js
@@ -28,6 +28,10 @@ foam.CLASS({
     'emoji'
   ],
 
+  constants: {
+    FORMATTER_CACHE: {}
+  },
+
   axioms: [
     {
       buildJavaClass: function(cls) {
@@ -156,6 +160,28 @@ foam.CLASS({
          * is not equal to the homeDenomination
          *
          */
+        // Not using foam locale as foam locale can be reset to a different value mid-session based on translation available
+        // using browser default formatting when available 
+        if ( navigator.language ) {
+          amount = Number(amount);
+          let opts = {
+            minimumFractionDigits: this.precision,
+            maximumFractionDigits: this.precision
+          }
+          if ( ! hideSymbol ) {
+            opts.style = 'currency';
+            opts.currency = this.id;
+          }
+          let formatterId = navigator.language + '-' + this.id;
+          if ( this.FORMATTER_CACHE[formatterId] ) {
+            return this.FORMATTER_CACHE[formatterId].format(amount);
+          } else {
+            let formatter = new Intl.NumberFormat(navigator.language, opts);
+            this.FORMATTER_CACHE[formatterId] = formatter;
+            return formatter.format(amount);
+          }
+        }
+        // TODO: Maybe consider deleting the custom formatting logic below
         amount = Math.round(amount);
         var isNegative = amount < 0;
         amount = amount.toString();

src/foam/u2/Element2.js
@@ -1934,6 +1934,17 @@ foam.CLASS({
   ]
 });
 
+foam.CLASS({
+  package: 'foam.u2',
+  name: 'DoubleUnitValueViewRefinement',
+  refines: 'foam.lang.DoubleUnitValue',
+  requires: [ 'foam.u2.view.CurrencyView' ],
+  properties: [
+    [ 'displayWidth', 15 ],
+    [ 'view', { class: 'foam.u2.view.CurrencyView', onKey: false } ]
+  ]
+});
+
 
 foam.CLASS({
   package: 'foam.u2',

src/foam/u2/table/UnstyledTableView.js
@@ -483,13 +483,13 @@ foam.CLASS({
           var buttonStyle = { label: '', buttonStyle: 'TERTIARY', size: 'SMALL' };
           return showPagination ?
           this.E().start(view.Cols).addClass(view.myClass('nav')).style({ 'justify-content': 'flex-end'}). // Have to do this here because Cols CSS is installed after nav. Investigate later
-            startContext({ data: view.scrollEl_ }).
+            startContext({ data: view.scrollEl_, controllerMode: foam.u2.ControllerMode.VIEW }).
               start(view.Cols).
                 style({ gap: '4px', 'box-sizing': 'border-box' }).
-                start('').add(view.scrollEl_$.dot('topRow')).addClass(this.myClass('counters')).end().
+                start('').tag(view.scrollEl_.TOP_ROW).addClass(this.myClass('counters')).end().
                 add('-').
-                start('').add(view.scrollEl_$.dot('bottomRow')).addClass(this.myClass('counters')).end().
-                start().addClass(view.myClass('separator')).translate(this.cls_.id + '.MESSAGE_OF', this.MESSAGE_OF).end().add(view.scrollEl_.daoCount$).
+                start('').tag(view.scrollEl_.BOTTOM_ROW).addClass(this.myClass('counters')).end().
+                start().addClass(view.myClass('separator')).translate(this.cls_.id + '.MESSAGE_OF', this.MESSAGE_OF).end().tag(view.scrollEl_.DAO_COUNT).
               end().
               start(view.scrollEl_.FIRST_PAGE, { ...buttonStyle, themeIcon: 'first' }).
               addClass(view.myClass('buttons')).end().

src/foam/u2/view/TableCellFormatter.js
@@ -61,6 +61,9 @@ foam.CLASS({
         return foam.u2.view.FnFormatter.create({
           class: 'foam.u2.view.FnFormatter',
           f: function(value, obj, axiom) {
+            if ( foam.Number.isInstance(value) ) {
+              value = Number(value).toLocaleString(navigator.locale);
+            }
             this.add(value);
           }
         });
@@ -233,6 +236,30 @@ foam.CLASS({
   package: 'foam.u2.view',
   name: 'UnitValueTableCellFormatterRefinement',
   refines: 'foam.lang.UnitValue',
+
+  properties: [
+    {
+      class: 'foam.u2.view.TableCellFormatter',
+      name: 'tableCellFormatter',
+      value: function(value, obj, axiom) {
+        var unitProp = obj.cls_.getAxiomByName(axiom.unitPropName);
+        if ( ! unitProp ) {
+          console.warn(obj.cls_.name, ' does not have the property: ', axiom.unitPropName);
+          this.add(value);
+          return;
+        }
+        var self = this;
+        this.startContext({objData: obj}).tag(foam.u2.view.ValueView, {prop: axiom, data: value}).endContext();
+      }
+    },
+    ['projectionSafe', false]
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.u2.view',
+  name: 'DoubleUnitValueTableCellFormatterRefinement',
+  refines: 'foam.lang.DoubleUnitValue',
 
   properties: [
     {

src/foam/u2/view/ValueView.js
@@ -42,14 +42,19 @@ foam.CLASS({
         this.add(
           unitPropSlot ?
           this.slot(function(data, unitProp) {
-            return prop.unitPropValueToString.call(self.__subContext__.objData, self.__subContext__, data, unitProp);
+            return prop.unitPropValueToString.call(self.__subContext__.objData, self.__subContext__, data, unitProp, prop.hideId);
           }, this.data$, unitPropSlot) :
           this.slot(function(data) {
-            return prop.unitPropValueToString.call(self.__subContext__.objData, self.__subContext__, data, self.__subContext__.objData[prop.unitPropName]);
+            return prop.unitPropValueToString.call(self.__subContext__.objData, self.__subContext__, data, self.__subContext__.objData[prop.unitPropName], prop.hideId);
           })
         );
       } else {
-        this.add(this.data$);
+        this.add(this.data$.map(v => {
+          if ( foam.Number.isInstance(v) ) {
+            return Number(v).toLocaleString(navigator.locale);
+          }
+          return v;
+        }));
       }
     }
   ]

commit f6c78b811cafcec16ab7075cfbf41fc093618236

PT-FIXES:DOS-4682 make the export 'Add Units' toggle strip units
The toggle has been broken almost forever. Its flag was ignored until
14d40ebfa7 (2024-03) wired it to Currency.format's hideId. Then
4213750d04 (#4773, 2026-02) broke it again: the new Intl.NumberFormat
path ignores hideId. Even while it worked, it only removed the ISO code
("GBP"), never the symbol. For DoubleUnitValue, born in that same PR,
it never worked at all.

Unchecked now means a plain number at the currency's precision
(1234.50). This is a new method, unitPropValueToPlainString, on purpose:
unitPropValueToString's 4th arg is used by GUI views as hideId, and
reusing it stripped the symbol from every table cell. addUnits is also
now passed through the single-object export path, which dropped it.

JSTest 8/8; every new assertion verified red without the fix.
Related: issue #5298 blames the same PR for breaking UnitValue.

src/foam/core/column/TableColumnOutputter.js
@@ -50,6 +50,9 @@ foam.CLASS({
             return stringArr.join(' ');
           }
           if ( prop.unitPropValueToString && unitPropName ) {
+            // 'Add Units' unchecked: bare spreadsheet-parseable number
+            if ( ! addUnitPropValueToStr && prop.unitPropValueToPlainString )
+              return await prop.unitPropValueToPlainString(x, val, unitPropName);
             return await prop.unitPropValueToString(x, val, unitPropName, ! addUnitPropValueToStr);
           }
           if ( foam.lang.DateTime.isInstance(prop) ) {
@@ -132,9 +135,9 @@ foam.CLASS({
     },
     {
       name: 'objectToTable',
-      code: async function(x, of, propNames, obj, lengthOfPrimaryPropsRequested) {
+      code: async function(x, of, propNames, obj, lengthOfPrimaryPropsRequested, addUnitPropValueToStr) {
         var values = await this.objToArrayOfStringValues(x, of, propNames, obj);
-        return this.returnTable(x, of, propNames, values, lengthOfPrimaryPropsRequested);
+        return this.returnTable(x, of, propNames, values, lengthOfPrimaryPropsRequested, addUnitPropValueToStr);
       }
     },
     {

src/foam/core/export/TableExportDriver.js
@@ -55,7 +55,7 @@ foam.CLASS({
       var propToColumnMapping  = this.columnConfigToPropertyConverter.returnPropertyColumnMappings(obj.cls_, propNames);
       var propertyNamesToQuery = this.columnHandler.returnPropNamesToQuery(propToColumnMapping);
 
-      return await this.outputter.objectToTable(X, obj.cls_, propertyNamesToQuery, obj, propNames.length);
+      return await this.outputter.objectToTable(X, obj.cls_, propertyNamesToQuery, obj, propNames.length, this.addUnits);
     },
 
     async function exportDAOAndReturnTable(X, dao, propNames) {

src/foam/lang/types.js
@@ -916,6 +916,21 @@ foam.CLASS({
         }
         return val;
       }
+    },
+    {
+      name: 'unitPropValueToPlainString',
+      documentation: `
+        Export with 'Add Units' unchecked: plain number at the currency's
+        precision so spreadsheets can parse and sum the column.
+      `,
+      value: async function(x, val, unitPropName) {
+        const currencyDAO = x.currencyDAO ?? this.__subContext__.currencyDAO;
+        if ( unitPropName && currencyDAO ) {
+          const unitProp = await currencyDAO.find(unitPropName);
+          if ( unitProp ) return unitProp.formatPrecision(val);
+        }
+        return val;
+      }
     }
   ]
 });
@@ -952,6 +967,26 @@ foam.CLASS({
           const unitProp = await currencyDAO.find(unitPropName);
           if ( unitProp )
             return unitProp.format(val, excludeUnit, false);
+        }
+        return val;
+      }
+    },
+    {
+      name: 'unitPropValueToPlainString',
+      documentation: `
+        Export with 'Add Units' unchecked: plain number at the currency's
+        precision so spreadsheets can parse and sum the column.
+        toFixed also collapses float noise; the '-' is stripped off -0.00.
+      `,
+      value: async function(x, val, unitPropName) {
+        const currencyDAO = x.currencyDAO ?? this.__subContext__.currencyDAO;
+        if ( unitPropName && currencyDAO ) {
+          const unitProp = await currencyDAO.find(unitPropName);
+          if ( unitProp ) {
+            var s = Number(val).toFixed(unitProp.precision);
+            if ( parseFloat(s) === 0 ) s = s.replace('-', '');
+            return s;
+          }
         }
         return val;
       }

src/foam/core/column/test/TableColumnOutputterJSTest.js
@@ -59,6 +59,29 @@ foam.CLASS({
         s = await format([ -6.984919309616089e-10, { id: 'GBP' } ]);
         x.test(! /e/i.test(s) && /0.00/.test(s),
           'float noise exports as rounded zero, not scientific notation: ' + s);
+
+        // 'Add Units' unchecked (addUnitPropValueToStr false): bare number a
+        // spreadsheet can parse — no symbol, no grouping, still 2dp
+        var bare = async values => (await outputter.arrayOfValuesToArrayOfStrings(ctx, props, [ values ], 1, false))[0][0];
+
+        s = await bare([ 1234.5, 'GBP' ]);
+        x.test(s === '1234.50',
+          'Add Units off exports bare number at currency precision: ' + s);
+
+        s = await bare([ -1234.5, 'GBP' ]);
+        x.test(s === '-1234.50',
+          'Add Units off keeps plain minus sign for negatives: ' + s);
+
+        s = await bare([ -6.984919309616089e-10, 'GBP' ]);
+        x.test(s === '0.00',
+          'Add Units off exports negative float noise as 0.00, not -0.00: ' + s);
+
+        // GUI path regression: ValueView passes prop.hideId (default true on
+        // DoubleUnitValue) as excludeUnit — that must keep the £ symbol,
+        // only the bare-number export flag strips formatting
+        s = await of.AMOUNT.unitPropValueToString(ctx, 1234.5, 'GBP', true);
+        x.test(/£/.test(String(s)),
+          'hideId=true from GUI views keeps the currency symbol: ' + s);
       }
     }
   ]
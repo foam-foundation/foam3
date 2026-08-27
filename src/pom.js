feb835ab63361de935aa5697972c76433e1d4b2d
Abdelaziz Mahdy <Abdelaziz.h.mahdy@gmail.com>
2026-08-27 13:31:01 -0300

    feat(dao): match the Java tree index bulk load in JS

--- src/pom.js
+++ src/pom.js
@@ -370,6 +370,7 @@ foam.POM({
     { name: "foam/dao/index/MDAOIndexKeyTest",                        flags: "js&test|java&test" },
     { name: "foam/dao/index/TreeNodeBench",                            flags: "js&test|java&test" },
     { name: "foam/dao/index/JournalReplayBench",                      flags: "js&test|java&test" },
+    { name: "foam/dao/index/MDAOBulkLoadTest",                        flags: "js&test|java&test" },
     { name: "foam/dao/MDAO",                                          flags: "js" },
     { name: "foam/dao/ArrayDAO",                                      flags: "js|java" },
     { name: "foam/dao/CopyOnWriteDAO",                                flags: "js|java" },

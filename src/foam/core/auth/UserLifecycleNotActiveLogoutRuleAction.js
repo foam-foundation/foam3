1ce2f595e7e8441479cd542b660356b0692bde75
Joel Hughes <joel@nanopay.net>
2026-09-03 22:45:16 -0400

    Logout users transitioning from LifecycleState ACTIVE.
    Can think of no reason to keep a now non-Active user logged in.

---
+++ src/foam/core/auth/UserLifecycleNotActiveLogoutRuleAction.js
@@ -0,0 +1,63 @@
+/**
+ * @license
+ * Copyright 2026 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+foam.CLASS({
+  package: 'foam.core.auth',
+  name: 'UserLifecycleNotActiveLogoutRuleAction',
+
+  documentation: `On transition from Active, logout user.
+Transition to DELETED is handled by UserLifecycleDeletedRuleAction`,
+
+  implements: [
+    'foam.core.ruler.RuleAction'
+  ],
+
+  javaImports: [
+    'foam.core.logger.Logger',
+    'foam.core.logger.Loggers',
+    'foam.core.session.Session',
+    'foam.dao.AbstractSink',
+    'foam.dao.DAO',
+    'foam.dao.Sink',
+    'foam.lang.ContextAgent',
+    'foam.lang.Detachable',
+    'foam.lang.X',
+    'static foam.mlang.MLang.*'
+  ],
+
+  methods: [
+    {
+      name: 'applyAction',
+      javaCode: `
+      final Logger logger = Loggers.logger(x, this);
+      agency.submit(x, new ContextAgent() {
+        public void execute(X x) {
+          User user = (User) obj;
+          // X rulerX = ruler.getX();
+
+          // log out by deleting session
+          try {
+            AuthService auth = (AuthService) x.get("auth");
+              ((DAO) x.get("sessionDAO")).where(
+                OR(
+                  EQ(Session.USER_ID, user.getId()),
+                  EQ(Session.AGENT_ID, user.getId())
+                )
+              ).select(new AbstractSink() {
+                public void put(Object obj, Detachable sub) {
+                  Session session = (Session) obj;
+                  auth.logout(session.getContext());
+                }
+              });
+          } catch (Throwable t) {
+            logger.error("Failed to logout", "user", user.getId(), t);
+          }
+        }
+      }, "UserLifecycleNotActiveLogoutRuleAction");
+      `
+    }
+  ]
+});

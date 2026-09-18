4c77fc78239cc34074b20cc15a585299d7d7e783
Abdelaziz Mahdy <Abdelaziz.h.mahdy@gmail.com>
2026-09-18 20:42:18 -0300

    Leave the mergeFObject change to #5519

--- src/foam/dao/AbstractF3FileJournal.js
+++ src/foam/dao/AbstractF3FileJournal.js
@@ -571,14 +571,7 @@ try {
     {
       name: 'mergeFObject',
       type: 'foam.lang.FObject',
-      documentation: `Merge the diff's set properties into the old object and
-        return the merged row.
-
-        When the classes match the merged old object is the row: a second pass
-        copying every set property back into the diff would only fire each
-        setter again, and on an update-heavy journal that pass was half the
-        serial apply cost. When the entry changed the row's class the diff, of
-        the new class, carries the old values instead.`,
+      documentation: 'Add diff property to old property',
       args: ['FObject oldFObject', 'FObject diffFObject' ],
       javaCode: `
         //get PropertyInfos
@@ -589,7 +582,6 @@ try {
           PropertyInfo prop = (PropertyInfo) e.next();
           mergeProperty(oldFObject, diffFObject, prop);
         }
-        if ( oldFObject.getClass() == diffFObject.getClass() ) return oldFObject;
         // it's backwards in case when we override the "class" was changed
         return diffFObject.copyFrom(oldFObject);
       `
